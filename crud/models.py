96d3b9eb4724831a85110240ce799cd098620f3f
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-10-22 12:41:30 +0545

    added model

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,14 @@
+from xml.parsers.expat import model
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    address = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+    
