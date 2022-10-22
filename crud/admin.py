cc9cbf947aa52d7af44bbfde467a18992337f453
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-10-22 12:52:26 +0545

    style admin

--- crud/admin.py
+++ crud/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Employee
 # Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'phone_number']
+    list_filter = ["address"]
+    search_fields = ['name']
+admin.site.register(Employee, EmployeeAdmin)
